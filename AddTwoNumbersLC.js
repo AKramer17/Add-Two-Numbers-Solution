113c5634848cd7f410f810d28c32a2168ff8101d
Alex Kramer <kramer612@gmail.com>
2019-09-05 14:25:21 -0400

    initial checks & balances

--- AddTwoNumbersLC.js
+++ AddTwoNumbersLC.js
@@ -13,7 +13,10 @@
 var addTwoNumbers = function(l1, l2) {
     let node = null;
     const carry = arguments[2];
-    while (l1 || l2) {
-
+    if (l1 || l2) {
+        const val1 = l1 ? l1.val : 0;
+        const val2 = l2 ? l2.val : 0;
+        const next1 = l1 ? l1.next : 0;
+        const next2 = l2 ? l2.next : 0;
     }
 };
